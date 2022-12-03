860ec684f76ccdeab56db67b271ac08a94dfdb64
nurnainisadir <nurnainisadir@gmail.com>
2022-12-03 23:09:20 +0700

    task fullstack

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -3,7 +3,7 @@
 @if(Auth::user()->role == 'admin')
 <div class="container-fluid" id="container-wrapper">
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Kelola user</h1>
+            <h1 class="h3 mb-0 text-gray-800">Kelola User</h1>
             <ol class="breadcrumb">
               <li class="breadcrumb-item"><a href="./">Home</a></li>
               <li class="breadcrumb-item active" aria-current="page">Manage User</li>
